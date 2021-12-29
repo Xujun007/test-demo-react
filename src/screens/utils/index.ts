9570963d6d5c2649d2dc73d3e754ab02dd1fe44e
jun.xu@rtdream.cn <648974419@qq.com>
2021-12-29 13:59:52 +0800

    修改为ts

---
+++ src/screens/utils/index.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (val: any) => (val === 0 ? false : !val);
+export const cleanObject = (object: object) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // @ts-ignore
+    const value = result[key];
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line
+  }, []);
+};
+
+export const useDebounce = (value: any, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearInterval(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
