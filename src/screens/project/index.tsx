097a3c7f3b390576e2aa6c0a72f521c5fe7a3199
Xujun007 <648974419@qq.com>
2022-02-04 15:37:03 +0800

    feat:路由配置

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import { KanbanScreen } from "../kanban";
+import { EpicScreen } from "../epic";
 
 export const ProjectScreen = () => {
-  return <h1>ProjectScreen</h1>;
+  return (
+    <div>
+      <Link to={"kanban"}>看板</Link>
+      <Link to={"epic"}>任务组</Link>
+      <Routes>
+        <Route path={"kanban"} element={<KanbanScreen />} />
+        <Route path={"epic"} element={<EpicScreen />} />
+        <Route index element={<KanbanScreen />} />
+      </Routes>
+    </div>
+  );
 };
